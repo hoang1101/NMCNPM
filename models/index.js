a95ea5bede56f28e230a4718d76b91e688a766b7
hoang1101 <lhhoang1101@gmail.com>
2023-12-02 23:38:19 +0700

    pusuuuu

--- models/index.js
+++ models/index.js
@@ -5,15 +5,27 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
+const env = "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+// sequelize = new Sequelize(
+//   config.database,
+//   "4VxzZhurnXSLgxd.root",
+//   config.password,
+//   config.port,
+//   config
+// );
+// // }
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
     host: config.host,
     dialect: config.dialect,
     port: config.port,
@@ -26,9 +38,8 @@ if (config.use_env_variable) {
     },
     timezone: "+07:00",
     logging: false,
-  });
-}
-
+  }
+);
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
